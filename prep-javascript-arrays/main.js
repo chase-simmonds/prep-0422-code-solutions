3c0d77a875dd77a1873c8d832a5c27d89f0eb152
Chase Simmonds <chasexsimmonds@gmail.com>
2022-04-27 01:49:09 +0000

    prep-javascript-arrays - Chase Simmonds

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,21 @@
+const languages = [
+  'html',
+  'css',
+  'javascript',
+  'react',
+  'node',
+  'postgres'
+];
+console.log(languages);
+
+const firstElement = languages[0];
+console.log('first element is ' + firstElement);
+
+const thirdElement = languages[2];
+console.log('third element is ' + thirdElement);
+
+const length = languages.length;
+console.log('the length is ' + length);
+
+const lastElement = languages[languages.length - 1];
+console.log('the last element is ' + lastElement);
